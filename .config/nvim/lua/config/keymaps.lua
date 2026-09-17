dba7242593c54c82497109ad09325a24737d6a0d
mrcs97 <313362664+mrcs97@users.noreply.github.com>
2026-09-17 21:13:39 +0530

    fix type

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -51,7 +51,7 @@ vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
 
 -- telescope
 local telescope = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', telecope.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', telescope.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', telescope.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', telescope.help_tags, { desc = 'Telescope help tags' })
